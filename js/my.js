3b384068087b9494d5ed9da4e79166e3c8c5bdbd
prgrmmrthz <izayoixd@gmail.com>
2021-09-25 16:09:28 +0800

    details modal

--- js/my.js
+++ js/my.js
@@ -5,8 +5,20 @@ $(document).ready(() => {
     listThat(data.Sheet1);
   });
 
+  $(document).on("click", ".viewDetails", function () {
+        var p_name = $(this).data('p_name');
+        var p_price = $(this).data('p_price');
+        var p_link = $(this).data('p_link');
+        var p_pix = $(this).data('p_pix');
+        //console.debug(x);
+        $('.p-name').text(p_name);
+        $('.p-price').text('₱'+Number(p_price).toFixed(2));
+        $('.p-link').attr("href", p_link);
+        $('.p-pix').attr("src", p_pix);
+  });
+
   function listThat(data){
-      console.debug(data);
+      //console.debug(data);
     $.each(data, (key, value)=>{
         //console.debug(key, value);
         $("#products .encl").append(`
@@ -33,8 +45,16 @@ $(document).ready(() => {
         </div>
         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
             <div class="text-center">
-            <a class="btn btn-outline-dark mt-auto" target="_blank" href="${value.link}"
+            <a class="btn btn-outline-dark mt-auto viewDetails"
+            data-p_name="${value.name}" data-p_price="${value.price}"
+            data-p_link="${value.link}" data-p_pix="${value.pic}"
+             data-bs-toggle="modal" data-bs-target="#detailsModal"
                 >Details</a
+            >
+            <a class="btn btn-outline-danger mt-auto" target="_blank" href="${
+              value.link
+            }"
+                >Buy Now!</a
             >
             </div>
         </div>
