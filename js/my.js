58973aaf6d9bb15941e49c63805cf252d0211657
prgrmmrthz <izayoixd@gmail.com>
2021-10-02 13:46:33 +0800

    new data 10-2-21 1 46 p,m

--- js/my.js
+++ js/my.js
@@ -1,27 +1,94 @@
 $(document).ready(() => {
+  let currentIndex = 0;
+  let coffeeDataLength = 0;
 
-  $.getJSON("https://raw.githubusercontent.com/prgrmmrthz/gsv3/main/productsList.json", function(data, status){
-    //console.debug(data);
-    listThat(data.Sheet1);
-  });
+  $.getJSON(
+    "https://raw.githubusercontent.com/prgrmmrthz/gsv3/main/productsList.json",
+    function (data, status) {
+      //console.debug(data);
+      coffeeDataLength = data.Sheet1.length;
+      //console.debug('cdl', coffeeDataLength);
+      listThat(data.Sheet1);
+    }
+  );
 
   $(document).on("click", ".viewDetails", function () {
-        var p_name = $(this).data('p_name');
-        var p_price = $(this).data('p_price');
-        var p_link = $(this).data('p_link');
-        var p_pix = $(this).data('p_pix');
-        //console.debug(x);
-        $('.p-name').text(p_name);
-        $('.p-price').text('₱'+Number(p_price).toFixed(2));
-        $('.p-link').attr("href", p_link);
-        $('.p-pix').attr("src", p_pix);
+    try {
+      currentIndex = $(this).data("p_key");
+      $("#ingredients").empty();
+      var p_name = $(this).data("p_name");
+      var p_price = $(this).data("p_price");
+      var p_link = $(this).data("p_link");
+      var p_pix = $(this).data("p_pix");
+      //console.debug(x);
+      $(".p-name").text(p_name);
+      $(".p-price").text("₱" + Number(p_price).toFixed(2));
+      $(".p-link").attr("href", p_link);
+      $(".p-pix").attr("src", p_pix);
+
+      $("#ingredients").append(`<small>
+        •Non-Dairy Creamer •premium coffee powder •tongkat ali •saw palmetto •moringa •ginseng •guyabano •barley grass •acai berry •grapeseed •agaricus mushroom
+            </small>
+        `);
+      const a = [
+        "Afghanistan;",
+        "Åland Islands;",
+        "Albania;",
+        "Algeria;",
+        "American Samoa;",
+        "Andorra;",
+        "Angola;",
+      ];
+      $(".p-pix").attr("src", p_pix);
+      //console.debug("currentIndex", currentIndex);
+    } catch (err) {
+      console.error(err);
+    }
+  }); //on click viewdetails
+
+  $(document).on("click", ".prev", function () {
+    try {
+        let z =0;
+        if(currentIndex === 0){
+            z=coffeeDataLength -1;
+        }else{
+            z = currentIndex - 1
+        }
+
+        //console.debug("target link", z);
+        const a = $(`.viewDetails[data-p_key=${z}]`);
+        //console.debug('key fetched',a.data("p_key"));
+        //$("#detailsModal").modal("hide");
+        a.trigger("click");
+    } catch (err) {
+      console.error(err);
+    }
   });
 
-  function listThat(data){
-      //console.debug(data);
-    $.each(data, (key, value)=>{
-        //console.debug(key, value);
-        $("#products .encl").append(`
+  $(document).on("click", ".next", function () {
+    try {
+        let z =0;
+        if(currentIndex === coffeeDataLength -1){
+            z=0;
+        }else{
+            z = currentIndex + 1
+        }
+
+        //console.debug("target link", z);
+        const a = $(`.viewDetails[data-p_key=${z}]`);
+        //console.debug('key fetched',a.data("p_key"));
+        //$("#detailsModal").modal("hide");
+        a.trigger("click");
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  function listThat(data) {
+    //console.debug(data);
+    $.each(data, (key, value) => {
+      //console.debug(key, value);
+      $("#products .encl").append(`
         <div class="col mb-5">
         <div class="card h-100">
         <img
@@ -45,7 +112,7 @@ $(document).ready(() => {
         </div>
         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
             <div class="text-center">
-            <a class="btn btn-outline-dark mt-auto viewDetails"
+            <a class="btn btn-outline-dark mt-auto viewDetails" data-p_key="${key}"
             data-p_name="${value.name}" data-p_price="${value.price}"
             data-p_link="${value.link}" data-p_pix="${value.pic}"
              data-bs-toggle="modal" data-bs-target="#detailsModal"
@@ -61,15 +128,15 @@ $(document).ready(() => {
         </div>
     </div>
         `);
-    });// each
-  }//function list that
+    }); // each
+  } //function list that
 
   $(document).on({
-    ajaxStart: function(){
-        $("body").addClass("loading");
+    ajaxStart: function () {
+      $("body").addClass("loading");
     },
-    ajaxStop: function(){
-        $("body").removeClass("loading");
-    }
-});
+    ajaxStop: function () {
+      $("body").removeClass("loading");
+    },
+  });
 });
